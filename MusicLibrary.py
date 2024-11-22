502a2ff8a5ac4c2aae2e54a9e0bca8c4e6c791df
Shiloah Servado <151849537+kyliaurr@users.noreply.github.com>
2024-11-23 07:54:11 +0800

    Update MusicLibrary.py

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -1,4 +1,15 @@
 from Track import Track
 
-class MusicLibrary:
-    pass
+class playlist:
+    def __init__(self, name):
+        self.name = name
+        self.playlist = []
+        
+    def new_track(self, track):
+        self.track.append(track)
+        
+    def view_playlist(self):
+        for index, playlist in enumerate(self.playlist, start=1):
+            print(f"{index}. {playlist}")
+            
+    def new_playlist(self, )
